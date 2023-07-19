3a2ece41dfe135029f4e2ffd4bac956b8e291651
christhecoolguy <christopherdiggs1@gmail.com>
2023-07-18 17:57:32 -0700

    2-cfix

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python3
-"""
-Write a script that starts a Flask web application:
+""" Starts a flask web app that returns a greeting """
 
-Your web application must be listening on 0.0.0.0, port 5000
-Routes:
-/: display “Hello HBNB!”
-/hbnb: display “HBNB”
-/c/<text>: display “C ” followed by the value of the text variable
-    (replace underscore _ symbols with a space )
-You must use the option strict_slashes=False in your route definition
-"""
+
 from flask import Flask
 
 app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def greeting():
+    """ function that returns greeting """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ function that returns a hbnb message """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ function that return message, but with a C """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
